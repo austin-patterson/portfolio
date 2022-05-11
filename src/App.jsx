936443f6607ea11f2eb4d8d0a3fff69505426a17
Austin Patterson <pattersonau@msoe.edu>
2022-05-11 12:59:10 -0500

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,99 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { useRef } from 'react'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { Container, Image, Nav, Navbar } from 'react-bootstrap'
+import feetupURL from './laptop-feet-up.jpg'
+import constructionURL from './construction-landscape.jpg'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // Globals
+  const ref = useRef()
+
+
+  // Components
+  const StickyNav = () => {
+
+    return (
+      <Navbar variant="dark" bg="dark" className="justify-content-center">
+        <Nav>
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar>
+    )
+  }
+
+  // const HomeSection = () => {
+
+  //   return (
+
+  //   )
+  // }
+
+  const ProjectCard = () => {
+
+
+    return (<></>)
+  }
+
+  // Page Contents
+  const numPages = 5
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Parallax pages={numPages}>
+        {/* Home banner image */}
+        <ParallaxLayer offset={0} speed={0.8} className="p-layer">
+          <div className="imgContainer">
+            <Image src={feetupURL} className="bannerImg"></Image>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.2} className="p-layer">
+          <div className="homeTextContainer">
+            {/* Name */}
+            <div className="name">
+              <span>Austin</span>
+              <span>Patterson</span>
+            </div>
+
+            {/* Tagline */}
+            <div className="tagline">
+              A happy programmer
+            </div>
+          </div>
+        </ParallaxLayer>
+
+        {/* TODO: Project Card 1 */}
+        <ParallaxLayer offset={1} speed={0.2} className="p-layer">
+          <h1>TODO - Project Card 1</h1>
+        </ParallaxLayer>
+
+        {/* TODO: Project Card 2 */}
+        <ParallaxLayer offset={2} speed={0.2} className="p-layer">
+          <h1>TODO - Project Card 2</h1>
+        </ParallaxLayer>
+
+        {/* TODO: About */}
+        <ParallaxLayer offset={3} speed={0.2} className="p-layer">
+          <h1>TODO - About</h1>
+        </ParallaxLayer>
+
+        {/* TODO: Contact */}
+        <ParallaxLayer offset={4} speed={0.3} className="p-layer">
+          <Image src={constructionURL} className="bannerImg"></Image>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={4} speed={-0.05} className="p-layer">
+          <h1>TODO - Contact Me</h1>
+        </ParallaxLayer>
+
+        <ParallaxLayer sticky={{ end: numPages }}>
+          <StickyNav></StickyNav>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   )
 }
